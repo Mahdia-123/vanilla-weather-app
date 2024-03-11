777f6526cf07e8b3fdad8ae101ae4b8bd0462210
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2024-03-11 12:40:39 +0430

    added the basic script

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
