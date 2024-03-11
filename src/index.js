31a54ec9a455b15f3d63ce9cc2e6e2c56cbd4b9e
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2024-03-11 14:53:39 +0430

    added the apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function searchCity(event) {
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", search);
+
+searchCity("toronto");
